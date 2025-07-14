468f004f650e8e08eaca49dbf35e186d1bbd12ac
zhengdechang <2473023641@qq.com>
2025-07-14 13:14:15 +0800

    update

--- react/next.config.ts
+++ react/next.config.ts
@@ -1,31 +1,7 @@
-/*
- * @Description:
- * @Author: Devin
- * @Date: 2025-07-14 10:23:35
- */
 import type { NextConfig } from "next";
 
-const isDev = process.env.NODE_ENV === "development";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-  // Only use basePath and assetPrefix in production
-  ...(isDev
-    ? {}
-    : {
-        basePath: "/react",
-        assetPrefix: "/react",
-      }),
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
